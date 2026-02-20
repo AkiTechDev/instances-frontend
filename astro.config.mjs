697f8b1faf859fbfda4046398b01a9a436c8b5e1
AkiTechDev <aki@aki-labs.com>
2026-02-20 02:13:31 +0000

    git deploy ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import qwikdev from "@qwikdev/astro";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://AkiTechDev.github.io',
+  base: '/instances-frontend',
   integrations: [qwikdev()]
 });
