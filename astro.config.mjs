6f51927d4c8fbccdcdfa9fa1169638a2c7128923
AkiTechDev <aki@aki-labs.com>
2026-02-24 02:24:21 +0000

    testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mkcert from 'vite-plugin-mkcert';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://AkiTechDev.github.io',
+  base: '/instances-frontend/',
   integrations: [qwikdev()],
 
   server: {
