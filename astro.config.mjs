6d0beb9eabb2a9b63abf7dc673a02bc3a8cb5e6c
AkiTechDev <aki@aki-labs.com>
2026-02-24 01:50:26 +0000

    fixed missing forward slash in base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mkcert from 'vite-plugin-mkcert';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://AkiTechDev.github.io',
-  base: '/instances-frontend',
+  base: '/instances-frontend/',
   integrations: [qwikdev()],
 
   server: {
