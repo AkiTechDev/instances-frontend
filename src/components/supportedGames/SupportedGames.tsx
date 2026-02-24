b9cfe39287d566c9fb746f72d56ff98512b54684
AkiTechDev <aki@aki-labs.com>
2026-02-24 01:41:05 +0000

    integrated some button interactivity

--- src/components/supportedGames/SupportedGames.tsx
+++ src/components/supportedGames/SupportedGames.tsx
@@ -1,7 +1,8 @@
-import { component$, Resource, useResource$, useStore } from "@builder.io/qwik";
+import { component$, Resource, useResource$, useSignal, useStore } from "@builder.io/qwik";
 
 import styles from "./supportedGames.module.css";
 import typo from "../../styles/typography.module.css";
+import buttonBig from "../../styles/components/buttonBig.module.css";
 
 interface Game {
     name: string
@@ -34,7 +35,17 @@ const supportedGames: {[id: string]: Game} = {
     },
 };
 
+function shuffleArray(array: string[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return 
+}
+
 const SupportedGameCard = component$(() => {
+    const showAll = useSignal(false)
     const games = useResource$(async () => {
         try {
             const resp = await fetch('https://api.instances.aki-labs.com/instances/types', {
@@ -48,6 +59,8 @@ const SupportedGameCard = component$(() => {
             const json = await resp.json();
             console.log("Got: ", JSON.stringify(json));
 
+            shuffleArray(json);
+
             return json as string[];
         } catch (error) {
             console.log(error);
@@ -57,22 +70,30 @@ const SupportedGameCard = component$(() => {
     })
 
     return (
+        <>
         <Resource
             value={games}
             onPending={() => <p>Loading ...</p>}
             onRejected={(error: any) => <p> Error: {error.message}</p>}
             onResolved={(games: string[]) => (
                 <div class={styles.gamesContainer}>
-                    {games.map((game, index) => (
+                    {(showAll.value ? games : games.slice(0, 4)).map((game, index) => (
                         <div class={styles.card}>
                             <img src={'/instances-frontend/imgs/' + game + '/banner.avif'} />
-                            <p class={typo.gameTitle}>{supportedGames[game].name}</p>
+                            <p class={typo.subTitle}>{supportedGames[game].name}</p>
                         </div>
                     ))}
                 </div>
             )}
         />
+        <button style="width:33%;" class={[buttonBig.buttonBig, buttonBig.transparentVibrantStyle]} onClick$={() => showAll.value = !showAll.value}><p class={typo.buttonText}>{showAll.value ? 'Show Less Games' : 'Show More Games'}</p></button>
+        </>
     )
 })
 
 export default SupportedGameCard
+
+
+const SupportedGameButton = component$(() => {
+    return null
+})
---
+++ src/lib/auth.ts
@@ -0,0 +1,80 @@
+import { EventType, LogLevel, PublicClientApplication } from "@azure/msal-browser";
+
+export const msalConfig = {
+    auth: {
+        clientId: 'b355e8f6-6973-4b89-821d-f6bada3cde4b',
+        authority: 'https://akilaboratoriesext.ciamlogin.com/f862f504-2fdc-4cb2-aed3-107ac3cb3154', // Replace the placeholder with your tenant subdomain 
+        redirectUri: 'https://instances.aki-labs.com/dashboard', // Points to window.location.origin. You must register this URI on Microsoft Entra admin center/App Registration.
+        postLogoutRedirectUri: '/', // Indicates the page to navigate after logout.
+        navigateToLoginRequestUrl: false, // If "true", will navigate back to the original request location before processing the auth code response.
+    },
+    cache: {
+        cacheLocation: 'sessionStorage',
+        storeAuthStateInCookies: false,
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback: (
+                level: LogLevel,
+                message: string,
+                containsPii: boolean
+            ): void => {
+                if (containsPii) {
+                    return;
+                }
+                switch (level) {
+                    case LogLevel.Error:
+                        console.error(message);
+                        return;
+                    case LogLevel.Info:
+                        console.info(message);
+                        return;
+                    case LogLevel.Verbose:
+                        console.debug(message);
+                        return;
+                    case LogLevel.Warning:
+                        console.warn(message);
+                        return;
+                }
+            },
+            piiLoggingEnabled: false,
+        },
+        windowHashTimeout: 60000,
+        iframeHashTimeout: 60000,
+        loadFrameTimeout: 0,
+    },
+}
+
+export const msalInstance = new PublicClientApplication(msalConfig);
+
+await msalInstance.initialize();
+
+msalInstance.addEventCallback((event) => {
+    if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
+        console.log("SIGNED IN");
+    }
+});
+
+export async function login() {
+    const accounts = msalInstance.getAllAccounts();
+    if (accounts.length > 0) {
+        msalInstance.setActiveAccount(accounts[0]);
+    } else {
+        await msalInstance.loginRedirect({
+            scopes: ["api://Instances/access"],
+        });
+    }
+}
+
+
+export async function signUp() {
+    const accounts = msalInstance.getAllAccounts();
+    if (accounts.length > 0) {
+        msalInstance.setActiveAccount(accounts[0]);
+    } else {
+        await msalInstance.loginRedirect({
+            scopes: ["api://Instances/access"],
+            prompt: "create",
+        });
+    }
+}
